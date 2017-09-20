e50a058c9e2a8bc59ced1e309c6e2e079c4a7b1d
sevvy <heleenseverin@hotmail.com>
2017-09-20 23:00:25 +0200

    killed some bugs, still more to go

--- App/classify_columns.py
+++ App/classify_columns.py
@@ -10,11 +10,11 @@ identical = 0
 col_types = [['marker_original', '(snp)|(marker[ -_]?(name)?)|(rs[ _-]?(id))', '((rs)[ _-]?)?\d+'],
              ['CHR', '(ch(r)?(omosome)?)', '[1-22]|[XY]'],
              ['BP', '(((pos)|(loc)|(bp))($)|([ _-]))?(hg(\d){2})?(grch(\d){2})*', '\d+'],
-             ['effect_allele', '(effect)|(ef)|(risk)|(aff)', '[ATCGDI]{1}'],
-             ['non_effect_allele', 'non[-_ ]effect|(un[ _-]?aff)', '[ATCGDI]{1}'],
+             ['effect_allele', '(effect)|(ef)|(risk)|(aff)', '[ACTGDI]{1}($|(\s))'],
+             ['non_effect_allele', 'non[-_ ]effect|(un[ _-]?aff)', '[ACTGDI]{1}($|(\s))'],
              ['major_allele', 'major', '[ATCGDI]{1}'],
              ['minor_allele', 'minor', '[ATCGDI]{1}'],
-             ['allele', '(allele(s)?)?(A([12_-]|$))?[12]?', '[ACTGDI]{1}'],
+             ['allele', '(allele(s)?)?(A([12_-]|$))?[12]?', '[ACTGDI]{1}($|(\s))'],
              ['freq', '(([12][ _-]?)?fr(e)?q(uency)?([ _-]?[12])?)', '(0)*\.\d*'],
              ['A1_freq', '((((effect)|(major))[ _-]?)fr(e)?q)?(EAF)?', '(0)*\.\d*'],
              ['A2_freq', '((((non[ -_]?effect)|(minor))[ _-]?)fr(e)?q)?(MAF)?', '(0)*\.\d*'],
@@ -40,21 +40,26 @@ def header_IDer(InputFile):
 
     def allele_check():
         # check if allele is already added to the headers list
+        #not being assigned for some reason
         if 'A1' in headers:
             headers[i] = 'A2'
         else:
             headers[i] = 'A1'
+        return headers
 
     def duplicate_check(headers):
+
         # if header is already in the headers list (for header in headers list except current header)
-        if col[0] in [x for i, x in enumerate(headers) if i != c]:
+        if col[0] in [x for n, x in enumerate(headers) if n != i]:
+            print(header)
             # if that duplicate header is "allele"
             if col[0] == 'allele' and 'A2' not in headers:
-                allele_check(headers)
+                allele_check()
             # if duplicate header is not allele, let user check the columns
             else:
                 headers = App.usr_check.init_usr_check(InputFile)
                 check_essential(headers, InputFile)
+        return headers
 
     #hope python knows this is an class object
     df = InputFile.file_to_df(chsize=1)
@@ -74,8 +79,9 @@ def header_IDer(InputFile):
             for c, col in enumerate(col_types):
                 if col_check(df, header, col[1], col[2]):
                     # call duplicate check for duplicate headers
-                    duplicate_check(headers)
+                    headers = duplicate_check(headers)
                     headers[i] = col[0]
+                    break
                 else:
                     dispose.append(i)
             break
@@ -103,9 +109,9 @@ def col_check(df, header, rehead, recol, head=False, col=False):
 
     # if 20 of 25 values match the column pattern, the type is confirmed
     cnt = 0
-    for row in df.itertuples():
-        row = str(row[0])
-        if colPattern.match(row):
+    for row, val in df.itertuples():
+        val = str(val)
+        if colPattern.match(val):
             cnt += 1
     if cnt > 20:
         col = True
@@ -117,7 +123,7 @@ def col_check(df, header, rehead, recol, head=False, col=False):
     elif col and colPattern.match(header):
         identical_increment()
     # if header or column matches with the pattern, header is confirmed
-    if head or col:
+    if head and col:
         #checker(df, head)
         return True
     else:
